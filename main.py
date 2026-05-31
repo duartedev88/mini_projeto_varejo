8bf78048e0fb253690bfd6374c627c4dd7865d75
Tiago Duarte <duartedev88@gmail.com>
2026-05-31 20:22:30 -0300

    atualização da main para executar a classe de limpeza

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from src.data_loader import DataLoader
+from src.data_cleaner import DataCleaner
 
+# ==========================================
+# CARREGAMENTO
+# ==========================================
 
 loader = DataLoader(
     "data/Base Varejo.csv"
@@ -8,3 +12,25 @@ loader = DataLoader(
 df = loader.load_data()
 
 loader.show_info(df)
+
+# ==========================================
+# LIMPEZA
+# ==========================================
+
+cleaner = DataCleaner(df)
+
+cleaner.show_nulls()
+
+cleaner.show_duplicates()
+
+cleaner.remove_empty_columns()
+
+cleaner.treat_categories()
+
+cleaner.convert_dates()
+
+cleaner.remove_duplicates()
+
+cleaner.remove_invalid_dates()
+
+cleaner.save_clean_data()
